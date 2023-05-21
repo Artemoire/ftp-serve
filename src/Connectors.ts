d303e1cd1c89afe8fa3d650a590c99fc9fc03c0c
Artemoire <dejan.tot@gmail.com>
2023-05-21 21:15:48 +0200

    feat: connector implementation

--- src/Connectors.ts
+++ src/Connectors.ts
@@ -3,41 +3,65 @@ import { PortAllocator } from "./PortAllocator";
 import { Timer } from "./Timer";
 
 export interface Connector {
-  connect(): Promise<Socket>
+  readonly host: string;
+  readonly port: number;
+  connect(): Promise<Socket | undefined>
 }
 
 export class PassiveConnector implements Connector {
 
   private server: Server;
-  private readonly closeServerOnIdle: Timer;
+  private dataConnectionPromise: Promise<Socket>;
+  private readonly portAllocator: PortAllocator = PortAllocator.range(49152, 65534); // TODO: allocator should be per network interface
+  public readonly host: string;
+  public readonly port: number;
 
   constructor(
-    private port: number,
-    private host: string
+    host: string
   ) {
-    console.log(`[DEBUG] Initializing passive mode on ${host}:${port}`);
+    this.host = host;
+    this.port = this.portAllocator.allocate();
+
     this.server = createServer();
-    this.server.listen(port, host);
-    this.closeServerOnIdle = new Timer(() => this.server.close(), 5000); // TODO: idle timeout config
-    this.closeServerOnIdle.start();
+    this.server.listen(this.port, this.host);
+    this.dataConnectionPromise = new Promise((resolve, reject) => {
+      this.server.once('connection', resolve);
+      setTimeout(reject, 5000); // TODO: timeout config
+    });
+
+    console.log(`[DEBUG] Server listening for a data connection on ${this.host}:${this.port}`);
   }
 
-  connect() {
-    this.closeServerOnIdle.cancel();
-    return new Promise<Socket>((resolve, reject) => {
-      const idleTimer = new Timer(() => this.server.close())
-      this.server.once('connection', (socket) => {
-        // socket.once('close', () => this.server.close()); // TODO: error handling
-        resolve(socket);
-      });
-    });
+  async connect(): Promise<Socket | undefined> {
+    // TODO: errors - Timeout, AddressInUse (in constructor ?)
+    // TODO: how handle client terminating the connection ?
+    try {
+      const sock = await this.dataConnectionPromise;
+      sock.once('end', () => this.cleanup());
+      return sock;
+    } catch (error) {
+      console.log('[ERROR] connection timeout');
+      this.cleanup();
+      // TODO: how does a server respond to a timeout ? (compliance testing case)
+    }
+  }
+
+  private cleanup() {
+    this.portAllocator.free(this.port);
+    this.server.close();
   }
 }
 
 export class ActiveConnector implements Connector {
 
-  connect(): Promise<Socket> {
-    throw new Error("Method not implemented.");
+  constructor(
+    public readonly host: string,
+    public readonly port: number = 20,
+  ) { }
+
+  async connect(): Promise<Socket | undefined> {
+    return undefined;
+    // throw new Error("Method not implemented.");
   }
 
 }
