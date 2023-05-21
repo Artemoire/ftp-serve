971f8d1528bec1e3363dd9bbd45004d5c0b48ae3
Artemoire <dejan.tot@gmail.com>
2023-05-21 21:16:41 +0200

    refactor: work dir ownership changed to dtp state

--- src/FTPSession.ts
+++ src/FTPSession.ts
@@ -5,7 +5,6 @@ import { DTP } from "./DTP";
 import { DefaultReplies } from "./replies/DefaultReplies";
 import { ServerReply } from "./replies/ServerReply";
 import { User } from "./state/User";
-import { WorkDir } from "./state/WorkDir";
 import { StorageClient } from "./storage/StorageClient";
 import { CommandParser } from "./transform/CommandParser";
 import { LineBuffer } from "./transform/LineBuffer";
@@ -15,7 +14,6 @@ export class FTPSession {
   public readonly user: User = new User();
   public readonly dtp = new DTP(this.socket.localAddress, this.socket.remoteAddress);
   public readonly storageClient: StorageClient | undefined;
-  public readonly workDir: WorkDir = new WorkDir();
 
   constructor(
     private socket: Socket
--- src/command/actions/pwd.ts
+++ src/command/actions/pwd.ts
@@ -5,5 +5,5 @@ import { registration } from "../commandRegistration";
 import { ServerReplyCode } from "../../replies/ServerReplyCode";
 
 registration.register(CommandMnemonic.PWD, async (session, parameter) => {
-  return session.reply(new MessageReply(ServerReplyCode.DirectoryOk, session.workDir.toString()));
+  return session.reply(new MessageReply(ServerReplyCode.DirectoryOk, `"${session.dtp.state.getWorkDir()}" is the current directory`));
 });
